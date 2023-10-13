a82d59b1357310aebfb6f76e02e7b9c174d932c2
eneamilo514 <eneamilo514@gmail.com>
2023-10-13 05:31:01 -0700

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	printf(and that piece of art is useful"/" - Dora Korpar, 2015-10-19\n)
+	putchar("and that piece of art is useful\"\ - Dora Korpar, 2015-10-19\n")
 	return (1);
 }
