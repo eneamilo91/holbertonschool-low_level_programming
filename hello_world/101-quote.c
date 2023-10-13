32e5f1e260a36f9e8e3e3adea7ffc2c1cf80bbd7
eneamilo514 <eneamilo514@gmail.com>
2023-10-13 05:38:57 -0700

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,8 +6,10 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+
+int main() 
 {
-	printf("and that piece of art is useful\"\ - Dora Korpar, 2015-10-19\n");
-	return (1);
+    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+    return 1; // Exit with an error code (non-zero) to indicate an error.
 }
+
