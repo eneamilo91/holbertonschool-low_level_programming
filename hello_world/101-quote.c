0c10be729ab0ee4551095c4526f38a1f167f936b
eneamilo514 <eneamilo514@gmail.com>
2023-10-15 12:38:34 -0700

    script 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,14 +1,15 @@
+#include <stdio.h>
 #include <unistd.h>
-
+#include <string.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-    write(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return (1);
+		char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+			write(1, str, strlen(str));
+				return (1);
 }
