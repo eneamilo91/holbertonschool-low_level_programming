fd7be5d8f57c497af2e18ead0fa51d1f93e7bfcd
eneamilo514 <eneamilo514@gmail.com>
2023-10-15 12:39:55 -0700

    script 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,8 +8,8 @@
  *     */
 int main(void)
 {
-		char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-			write(1, str, strlen(str));
-				return (1);
+	write(1, str, strlen(str));
+	return (1);
 }
