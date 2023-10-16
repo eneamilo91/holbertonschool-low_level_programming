2c6ee7ccdae71e58d9bb13c212870a8e2364d34c
eneamilo514 <eneamilo514@gmail.com>
2023-10-16 08:32:07 -0700

    script 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function is the entry point for the program.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	char alphabet[] = {
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+		'u', 'v', 'w', 'x', 'y', 'z'
+ 	};
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (i==4 || i==16)
+			continue;
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
