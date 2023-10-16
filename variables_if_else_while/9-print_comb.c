62b26145fb41dbf96e99981c1559ab5d513da9cf
eneamilo514 <eneamilo514@gmail.com>
2023-10-16 15:55:46 -0700

    script 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,16 +10,15 @@
 int main(void)
 {
 	int i;
-	int j;
+	
 	
 	for (i = 0; i <= 9; i++)
 {
-		for (j = 0; j < 10; j++)
-{			putchar(i + '0');
-			putchar(j + '0');
+		
+			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
-}
+
 }
 
 	return (0);
