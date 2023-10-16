879e071fce9c4ecf9052e75c036ebcb32b184ade
eneamilo514 <eneamilo514@gmail.com>
2023-10-16 01:56:18 -0700

    script 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - entry point
- * Return: 0
+ * main - Entry point of the program.
+ *
+ * This function is the entry point for the program. It generates a random number,
+ * calculates the last digit of that number, and then prints the result.
+ *
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
-        int n;
+	int n;
 	int ld;
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	ld = n % 10;
-        if (ld > 5)
-                printf("Last digit of %d is %d and is greater than 5\n", n,ld);
-        else if (ld==0)
-                printf("Last digit of %d is %d and is 0\n", n,ld);
-        else
-                printf("Last digit of %d is %d and is less than 6 and not 0\n", n,ld);
-        return (0);
+
+	if (ld > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+	else if (ld == 0)
+		printf("Last digit of %d is %d and is 0\n", n, ld);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+
+	return (0);
 }
+
+
