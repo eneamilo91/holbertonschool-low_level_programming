945a5a31e98c62931b0522b50207f18d04305850
eneamilo514 <eneamilo514@gmail.com>
2023-10-16 01:32:05 -0700

    script 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@ int main(void)
 
         srand(time(0));
         n = rand() - RAND_MAX / 2;
-	int ld = n%10;
+	int ld = n % 10;
         if (ld > 5)
                 printf("Last digit of %d is %d and is greater than 5\n", n,ld);
         else if (ld==0)
