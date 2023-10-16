e94111c83ba413a1bd9108d1ba6b17b90814b17e
eneamilo514 <eneamilo514@gmail.com>
2023-10-16 02:27:46 -0700

    script 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function is the entry point for the program.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+    char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    int i;
+    
+    for (i = 0; i < 26; i++)
+    {
+        putchar(alphabet[i]);
+        putchar('\n');
+    }
+
+    return (0);
+}
+
+
+
