9f7d55ebdc1de65a9b2188b58b77c26ca37d3daf
eneamilo514 <eneamilo514@gmail.com>
2023-10-16 14:55:28 -0700

    script 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function is the entry point for the program.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	char hexa[] = "0123456789abcdef";   
+	int x;
+
+	for (x = 0; x < 16; x++)
+{
+		putchar(hexa[x]);
+		
+}
+
+	putchar('\n');
+
+	return (0);
+}
+
