89f01ffb98ec9a2ed681ce26ba9ddba81ff7d4f9
eneamilo514 <eneamilo514@gmail.com>
2023-10-16 09:10:06 -0700

    script 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function is the entry point for the program.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int x = 0;
+	while (x <= 9)
+{	
+	printf("%d",x);
+		x++;
+}
+
+
+
+	return (0);
+}
+
