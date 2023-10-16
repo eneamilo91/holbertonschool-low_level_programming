f4539bfdf3497036be4bd671199c7ad5acbfb6be
eneamilo514 <eneamilo514@gmail.com>
2023-10-16 08:55:30 -0700

    script 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -13,12 +13,12 @@ int main(void)
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
 		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
 		'u', 'v', 'w', 'x', 'y', 'z'
- 	};
+	};
 	int i;
 
 	for (i = 0; i < 26; i++)
 	{
-		if (i==4 || i==16)
+		if (i == 4 || i == 16)
 			continue;
 		putchar(alphabet[i]);
 	}
