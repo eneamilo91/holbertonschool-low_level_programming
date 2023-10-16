12393f427c499fda712ea8573bd75430e232e7b6
eneamilo514 <eneamilo514@gmail.com>
2023-10-16 03:01:22 -0700

    script 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -9,14 +9,19 @@
  */
 int main(void)
 {
-    char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' , 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    int i;
+	char alphabet[] = {
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+		'u', 'v', 'w', 'x', 'y', 'z'
+	};
+	int i;
 
-    for (i = 0; i < 26; i++)
-    {
-        putchar(alphabet[i]);
-    }
-    putchar('\n');
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
+
