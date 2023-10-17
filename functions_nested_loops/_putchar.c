73ce992b44e00443b408e717c17b9e7687d9be07
eneamilo514 <eneamilo514@gmail.com>
2023-10-17 14:27:28 -0700

    script 0

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include<unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-
-#endif 
+int _putchar(char c); 
