a8050554a0a761f8126372d8978e7386cf788af4
eneamilo514 <eneamilo514@gmail.com>
2023-10-17 13:44:09 -0700

    script 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,7 @@
+#include<main.h>
+
+int main(void)
+
+{
+	_putchar(char c);
+}
---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function is the entry point for the program.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int i;
+	int j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0;j <= 9;j++){
+			putchar(i + '0')
+			putchar(j + '0')
+	
+	}
+	putchar('\n');
+	return (0);
+}
