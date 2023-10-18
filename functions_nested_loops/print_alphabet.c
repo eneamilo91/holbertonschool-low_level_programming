b648105a6106f817ce5beabd6e3cacd3bf711d0a
eneamilo514 <eneamilo514@gmail.com>
2023-10-18 02:02:23 -0700

    script 1

---
+++ functions_nested_loops/print_alphabet.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z';letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+
+
+}
