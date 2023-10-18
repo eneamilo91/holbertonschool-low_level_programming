ba5c186ebed970ce92735fcf8a9bc8980b65e0bb
eneamilo514 <eneamilo514@gmail.com>
2023-10-18 03:02:03 -0700

    script 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,10 @@ void print_alphabet_x10(void)
 
 {
 	char letter;
-	
-	for (letter = 'a'; letter <= 'z' ; letter++)
-		_putchar(letter * 10);
+	int x;
+	for(x = 0 ; x <= 10; x++)
+		for (letter = 'a'; letter <= 'z' ; letter++)
+		_putchar(letter);
 		_putchar('\n');
 
 }
