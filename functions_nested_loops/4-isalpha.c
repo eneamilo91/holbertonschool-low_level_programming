f7ba8c4a1505327c6c61eef82254566456176741
eneamilo514 <eneamilo514@gmail.com>
2023-10-18 04:45:02 -0700

    script 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+
+{
+	if (97 <= c &&  122 >= c) && (65 <= c && 90 >= c) 
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
