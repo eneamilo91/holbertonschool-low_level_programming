5f4a4de15a1f30138c383e9cc7ec46013fc9b148
eneamilo514 <eneamilo514@gmail.com>
2023-10-18 14:33:29 -0700

    script 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @x: the int to extract the last digit from
+ * Return: value of the last digit
+ */
+
+	int print_last_digit(int x)
+{
+	if (x >= 0)
+	{
+		return (x % 10);
+	}
+	else
+	{
+		return (-(x % 10));
+	}
+
+}
