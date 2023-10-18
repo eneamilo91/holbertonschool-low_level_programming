8c4a8da640c37bf7b9d90bc0e19af96a85501a36
eneamilo514 <eneamilo514@gmail.com>
2023-10-18 05:13:01 -0700

    script 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include"main.h"
 
 /**
- * _islower - checks for lowercase character
+ * _isalpha - checks for an alphabetic character; in the standard "C" locale
  * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
 
 int _isalpha(int c)
