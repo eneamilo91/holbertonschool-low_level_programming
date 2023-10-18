6d93a6574139cd9e4d48bda3432c6a0197cb26d3
eneamilo514 <eneamilo514@gmail.com>
2023-10-18 13:51:12 -0700

    script 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -5,11 +5,11 @@
  * @n: the character to check
  * Return: 1 n is greater than zero, 0 if n is zero, -1 if n is less then zero
  */
-	
+
 	int print_sign(int n)
 
 {
-	
+
 	if (n > 0)
 	{
 		_putchar('+');
@@ -18,7 +18,7 @@
 	else if (n == 0)
 	{
 		_putchar('0');
-		return (0);	
+		return (0);
 	}
 	else
 	{
