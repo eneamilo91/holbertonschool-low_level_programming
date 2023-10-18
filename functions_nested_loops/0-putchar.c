0ef9659478fad7b696cb5d4538abb98cb0479a1d
eneamilo514 <eneamilo514@gmail.com>
2023-10-18 01:42:19 -0700

    script 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,11 +6,17 @@
  */
 int main(void)
 {
-	int i;
-	char str[] = "_putchar";
-		for (i = 0 ; i <= 8; i++)
-			_putchar(str[i]);
-		_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
+
+	
 }
