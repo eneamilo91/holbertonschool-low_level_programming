25c544e4c56047aac31990dd30812fa84607c0b0
eneamilo514 <eneamilo514@gmail.com>
2023-10-18 14:14:36 -0700

    script 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,8 +2,8 @@
 
 /**
  * _abs - function computes the absolute value of the integer argument j
- * @n: the character to check
- * Return: 1 n is greater or equal zero,  -1 if n is less then zero
+ * @a: the character to check
+ * Return: 1 a is greater or equal zero,  -1 if a is less then zero
  */
 	
 	int _abs(int a)
