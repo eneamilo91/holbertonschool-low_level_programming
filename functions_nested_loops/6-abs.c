e374fe5b7ba58dbcd5ca4d679546f4d63f6a8db9
eneamilo514 <eneamilo514@gmail.com>
2023-10-18 14:04:02 -0700

    script 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - function computes the absolute value of the integer argument j
+ * @n: the character to check
+ * Return: 1 n is greater or equal zero,  -1 if n is less then zero
+ */
+	
+	int _abs(int)
+{
+	int a;
+
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (-a);
+	}
+
+}
