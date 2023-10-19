0b8bdc9cc81b0014a5ad19dcf99a381770f3aa45
eneamilo514 <eneamilo514@gmail.com>
2023-10-19 00:46:49 -0700

    script 10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,10 +3,9 @@
 #include "main.h"
 /**
  * add - Entry point
- * @first_x: -first number
- * @second_y: second num
- * Return the sum of two int numbers;
- * Return: 0
+ * @first_x:int
+ * @second_y: int
+ * Return the sum of two int numbers
  */
 
 	int add(int x, int y)
