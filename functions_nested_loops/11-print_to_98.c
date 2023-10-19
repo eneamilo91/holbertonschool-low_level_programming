daa9ca7cff4079ccf3adce0b2ca2819d26a04d13
eneamilo514 <eneamilo514@gmail.com>
2023-10-18 17:18:35 -0700

    script 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+	void print_to_98(int n, int x)
+{
+
+
+	for (n = x;x <= 98;x++)
+	{
+		if (x != 98)
+		{
+
+			_putchar(',');
+			_putchar(' ');
+		}
+
+		_putchar(x + '0');
+		_putchar('\n');
+
+	}
+	for (n = x;x >=98;x--)
+	{
+		if (x != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar(x + '0');
+		_putchar('\n');
+	}
+
+}
