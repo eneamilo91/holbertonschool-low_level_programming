dab1f4989f9f4692224fdd9cf287d38311c0375b
eneamilo514 <eneamilo514@gmail.com>
2023-10-20 04:28:38 -0700

    file 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,10 @@
 #include"main.h"
 
+/**
+ * _isupper - check for uppercase letters
+ * @c: -variable int c check character
+ * Return: 0 or 1
+ */
 int _isupper(int c)
 {
 	if ((c >= 65 && c <= 90))
