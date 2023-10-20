99ff7b9656ffdd656a0a2eec5beb2a6ecaf56828
eneamilo514 <eneamilo514@gmail.com>
2023-10-20 06:49:22 -0700

    file 2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,5 +1,11 @@
 #include"main.h"
 
+/**
+ * mul -entry point multiply two integers
+ * @a: -holds value for input
+ * @b: -hold value for input
+ * Return: a*b
+ * */
 int mul(int a, int b)
 {
 	return (a * b);
