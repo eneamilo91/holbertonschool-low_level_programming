745044b852af43d6f4de556749c519a5d07fa5ee
eneamilo514 <eneamilo514@gmail.com>
2023-10-20 02:07:48 -0700

    file 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,14 +4,14 @@ void more_numbers(void)
 {
 	int a;
 	int b;
-	
-	for (a = 0 ; a < 10 ;a++)
+
+	for (a = 0 ; a < 10 ; a++)
 	{
-		for (b = 0 ;b <= 14 ;b++)
+		for (b = 0 ; b <= 14 ; b++)
 		{
 			if (b > 9)
 			{
-	
+
 				_putchar(b / 10 + '0');
 
 			}
