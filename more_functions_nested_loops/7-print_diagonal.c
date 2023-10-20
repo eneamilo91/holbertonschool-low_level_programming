8a1a6e84563255d18dc4fa58b2b62b06fea344cc
eneamilo514 <eneamilo514@gmail.com>
2023-10-20 07:39:42 -0700

    file 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - function print line
+ * @n: - parameter from input until when print \
+ * main.h header file
+ * Return: Void
+ */
 
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
