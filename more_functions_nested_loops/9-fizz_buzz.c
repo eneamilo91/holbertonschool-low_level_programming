9d5dd3fe6e826ab21330adc9cd6807e7f78523b1
eneamilo514 <eneamilo514@gmail.com>
2023-10-21 09:24:53 -0700

    file 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -5,32 +5,32 @@
  * Return: 0
  */
 
-	
-        int main(void)
-{
-        int x;
 
-         for(x = 1; x <= 100; x++)
+	int main(void)
+{
+	int x;
+
+	for(x = 1; x <= 100; x++)
     {
-                if (x % 3 == 0 && x % 5 == 0)
+	if (x % 3 == 0 && x % 5 == 0)
         {
-                        printf("FizzBuzz");
+			printf("FizzBuzz");
                         printf(" ");
         }
 
-                else if (x % 5 == 0)
+			else if (x % 5 == 0)
         {
                         printf("Buzz");
                        	printf(" ");
         }
 
-                else if (x % 3 == 0 )
+			else if (x % 3 == 0 )
         {
                         printf("Fizz");
                         printf(" ");
         }
 
-                else
+			else
         {
                         printf("%d", x);
                         printf(" ");
@@ -38,5 +38,5 @@
 
     }
    
-   return (0);
+	return (0);
 }
