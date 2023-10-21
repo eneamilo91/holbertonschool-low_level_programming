3a03d06864da045205a5a58697d8515da27eeb45
eneamilo514 <eneamilo514@gmail.com>
2023-10-21 09:03:41 -0700

    file 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * fizzBuzz -entry point,prints numbers 0 - 100
+ */
+
+        void FizzBuzz(void)
+{
+        int x;
+
+         for(x = 1; x <= 100; x++)
+    {
+                if (x % 3 == 0 && x % 5 == 0)
+        {
+                        printf("FizzBuzz");
+                        putchar(' ');
+        }
+
+                else if (x % 5 == 0)
+        {
+                        printf("Buzz");
+                        putchar(' ');
+        }
+
+                else if (x % 3 == 0 )
+        {
+                        printf("Fizz");
+                        putchar(' ');
+        }
+
+                else
+        {
+                        printf("%d", x);
+                        putchar(' ');
+        }
+
+    }
+   putchar('\n');
+}
