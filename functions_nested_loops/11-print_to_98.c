4271d54e2b672c6a4be51da10e943d8eb5e7e19c
eneamilo514 <eneamilo514@gmail.com>
2023-10-21 07:19:34 -0700

    file 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,33 +1,29 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _islower - checks for lowercase character
+ * print_to_98 -entry point,check for chars 
  * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
- */
+ */ 
 
 	void print_to_98(int n)
 {
-	if(n > 98)
+	if (n > 98)
 	{
-		while(n > 98)
+		while (n > 98)
 		{
-			printf("%d, ", n);
+			printf("%d,", n);
 			n--;
 		}
 	}
 
-	if(n < 98)
+	if (n < 98)
 	{
 		while (n < 98)
 		{
-			printf("%d, ", n);
+			printf("%d,", n);
 			n++;
 		}
 	}
 
 	printf("98\n");
 }
-
-
-	
