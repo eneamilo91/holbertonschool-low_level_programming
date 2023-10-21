7ff7a1b9f74a99a2effb06091cd71da92b98fc97
eneamilo514 <eneamilo514@gmail.com>
2023-10-21 13:28:39 -0700

    file 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include"main.h"
+
+/**
+ * print_triangle -entry point,prints a triangle using char
+ * @size: -hold the value for output
+ */
+
+	void print_triangle(int size)
+{	
+	int a=0;
+	int b=0;
+	int y;
+
+	if (size > 0)
+	{
+		for (y = 1; y <= size; y++)
+		{
+			for (a = 0; a < size - 1 - b;a++)
+			{
+				_putchar(' ');
+			
+			}
+			for(b = 0; b <= size - 1 - a;b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		} 
+	}
+	else
+		_putchar('\n');
+}
