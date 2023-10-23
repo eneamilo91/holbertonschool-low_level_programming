3ce943edc803a309742d7cba4a19fdd5c4bb4b7a
eneamilo514 <eneamilo514@gmail.com>
2023-10-23 15:26:05 -0700

    file 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,14 +10,13 @@
 
 	void print_array(int *a, int n)
 {
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++)											{
 		printf("%d", a[i]);
 	if (i < n - 1)
 	{
 		printf(", ");
 	}
-	}
+															}
 	printf("\n");
 }
 
