03fd44a490bcfa6b1ad9115b6ce9a701a7d7717d
eneamilo514 <eneamilo514@gmail.com>
2023-10-23 14:51:36 -0700

    file 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,19 @@
 #include"main.h"
 
+/**
+ * puts2 -entry point,prints every other char
+ * @str: -holds value for input
+ */ 
+
 
 	void puts2(char *str)
 {
 	int i;
-	
-	for (i = 0;str[i] != '\0'; i = i + 2)
+
+	for (i = 0; str[i] != '\0'; i = i + 2)
 	{
 		_putchar(str[i]);
-	
+
 	}
 
 	_putchar('\n');
