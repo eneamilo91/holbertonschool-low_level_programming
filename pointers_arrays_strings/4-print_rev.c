f2569773f4eef3849dd1b522bc442fe10de55624
eneamilo514 <eneamilo514@gmail.com>
2023-10-23 04:45:06 -0700

    file 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+
+	void print_rev(char *s)
+{
+	int len = 0;
+       	int i,last_index;
+	while (s[len] != '\0')
+		len++;
+		last_index = len - 1;
+
+	for (i = last_index; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
