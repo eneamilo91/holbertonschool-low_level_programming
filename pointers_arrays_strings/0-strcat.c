5db82dc6a1c4cad523b6c91ed170ea0a57c0f56e
eneamilo514 <eneamilo514@gmail.com>
2023-10-24 14:31:29 -0700

    file 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -7,14 +7,14 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 	char *ptr;
-	ptr = &dest;
+	ptr = dest;
 
-	for (i = 0; dest[i] == '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 	{
 	
 	}
 	j=i;
-	while (j != '\n')
+	while (src[j] != '\0')
 	{
 		_putchar('src[j]');
 		j++;
