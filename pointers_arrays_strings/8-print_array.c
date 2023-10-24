8dab8ade9084dc9f6c9d59d58f5f7cf068294beb
eneamilo514 <eneamilo514@gmail.com>
2023-10-24 02:03:52 -0700

    file 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -4,21 +4,19 @@
  * print_array -entry point,prints elements of array
  * @a: -points to value for input
  * @n: -hold value for input
- */ 
+ */
 
+#include <stdio.h>
 
-
-	void print_array(int *a, int n)
+void print_array(int *a, int n)
 {
-    int i;
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", a[i]);
-        if (i < n - 1)
-        {
-            printf(", ");
-        }
-    }
-    printf("\n");
-}
+	int i;
 
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
