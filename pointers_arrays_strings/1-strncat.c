2bc7f39548626b136a51c2f81af2c40bc9477fd4
eneamilo514 <eneamilo514@gmail.com>
2023-10-24 14:52:11 -0700

    file 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+    int i;
+    int j;
+    char *ptr;
+    ptr = dest;
+
+    
+    for (i = 0; dest[i] != '\0'; i++)
+    {
+    }
+
+    j = 0;
+    
+    while (src[j] != '\0')
+    {
+        dest[i + j] = src[j];
+        j++;
+    }
+
+	if (j >= n)
+		return ptr;
+}
+
