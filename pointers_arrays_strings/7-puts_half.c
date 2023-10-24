203b7fa81348bbca4130163df94b00b69ddcb95a
eneamilo514 <eneamilo514@gmail.com>
2023-10-24 01:24:03 -0700

    file 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+
+	void puts_half(char *str)
+{
+	int i;
+	int j;
+
+	for (i = 0;str[i] != 0; i++)
+	{
+
+	}
+	j= (i - 1) / 2
+
+	for (j >= (i - 1) / 2; str[j] != '\0'; j++)
+	{
+		_putchar(str[j]);
+	
+	}
+	if(j % 2 != 0)
+	{
+		_putchar(str[j]);
+	}
+}
