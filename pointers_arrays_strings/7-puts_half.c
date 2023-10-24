5d10bf653e83103f4aae7fe656d0967a7f97505c
eneamilo514 <eneamilo514@gmail.com>
2023-10-24 01:51:27 -0700

    file 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,20 +1,25 @@
 #include"main.h"
 
+/**
+ * puts_half -entry point,prints half s
+ * @str: -holds value for input
+ */ 
 
-	void puts_half(char *str)
+
+void puts_half(char *str)
 {
 	int i;
 	int j;
 
-	for (i = 0;str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 
 	}
-	j= (i - 1) / 2;
+	j = (i - 1) / 2;
 
 	for (j = (i - 1) / 2 + 1; str[j] != '\0'; j++)
 	{
-        		_putchar(str[j]);
-     	}
+		_putchar(str[j]);
+	}
 	_putchar('\n');
 }
