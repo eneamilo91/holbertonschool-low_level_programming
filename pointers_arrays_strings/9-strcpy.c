69a3973cc16b9e48670c20302bc5e511f1e33140
eneamilo514 <eneamilo514@gmail.com>
2023-10-24 02:08:48 -0700

    file 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,12 @@
 #include"main.h"
 
+/**
+ * _strcpy -entry point,copys two strings
+ * @dest: -points to value for input
+ * @src: -points to value for input
+ * Return: ptr
+ */
+
 
 char *_strcpy(char *dest, char *src)
 {
