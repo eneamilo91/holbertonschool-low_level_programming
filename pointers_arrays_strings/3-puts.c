3b9f88ccabf417b2c3ef8e3a9758b5866849af52
eneamilo514 <eneamilo514@gmail.com>
2023-10-24 00:37:15 -0700

    file 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,14 +1,18 @@
 #include"main.h"
+/**
+ * _puts -entry point,prints a newline  f string
+ * @str:-holds value or input
+ */
 
 
 	void _puts(char *str)
 {
 	int i;
 
-	for (i = 0 ; str[i]!= '\0'; i++)
+	for (i = 0 ; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);			
-	
+		_putchar(str[i]);
+
 	}
 _putchar('\n');
 }
