2c2b4995dfb8ed401bda88899e67c47a0b3bd8f4
eneamilo514 <eneamilo514@gmail.com>
2023-10-25 09:30:22 -0700

    file 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -2,6 +2,7 @@
 
 /**
  * _strncat -entry point,conc two strings
+ * @dest: -holds value for input
  * @src: -holds value for input
  * @n: -holds value for input
  * Return: (ptr)
@@ -28,7 +29,7 @@ char *_strncat(char *dest, char *src, int n)
 	j++;
 	}
 
-	dest[i + j] = '\0'; 
+	dest[i + j] = '\0';
 
 	return (ptr);
 }
