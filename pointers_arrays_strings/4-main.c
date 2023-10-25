5fa838bafea39cac4a855526ae8058184d8a7d11
eneamilo514 <eneamilo514@gmail.com>
2023-10-25 10:50:59 -0700

    file 4

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -22,20 +22,5 @@ void print_array(int *a, int n)
         printf("%d", a[i]);
         i++;
     }
-    printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
-    return (0);
+    printf("\n");}
 }
