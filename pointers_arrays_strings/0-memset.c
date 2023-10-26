ae6d4b4e31d5361f85bdb1ce6b1394a20f4bfe71
eneamilo514 <eneamilo514@gmail.com>
2023-10-26 14:38:48 -0700

    file 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,14 +1,22 @@
 #include"main.h"
 
+/**
+ * _memset -entry point,fills memory
+ * @s: -points to specific array
+ * @b: -holds value for output
+ * @n: -holds value for output
+ * Return: (s)
+ */ 
+
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
-	
+
 	}
 	return (s);
 
