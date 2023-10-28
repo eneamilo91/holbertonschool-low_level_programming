3d49782373a0b43e6b00fad42ec3d6a66d9df13c
eneamilo514 <eneamilo514@gmail.com>
2023-10-28 14:11:34 -0700

    file 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,25 +1,31 @@
 #include"main.h"
+/**
+ * _strspn -entry point,gets the length of prefixed str
+ * @s: -holds value for output
+ * @accept: -holds value for output
+ * Return: (count)
+ */
 
 unsigned int _strspn(char *s, char *accept)
 {
 	int i;
-    int j;
-    int count = 0;
+	int j;
+	int count = 0;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-            {
-                count++;
-                break;
-            }
-        }
-        if (accept[j] == '\0')
-        {
-            break;
-        }
-    }
-    return (count);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+		break;
+		}
+	}
+    	return (count);
 }
