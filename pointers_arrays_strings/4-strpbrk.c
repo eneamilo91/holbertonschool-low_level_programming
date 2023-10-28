4ac5db5e5b56ff436c87ada3524e853a605005da
eneamilo514 <eneamilo514@gmail.com>
2023-10-28 14:43:02 -0700

    file 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include"main.h"
+#include <stddef.h>
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		
+		}
+
+	}
+	return (NULL);
+
+}
