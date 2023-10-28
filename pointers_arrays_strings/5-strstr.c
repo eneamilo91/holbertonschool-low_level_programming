75b899f4122afbce7e1c20b991acde4bb5be66ec
eneamilo514 <eneamilo514@gmail.com>
2023-10-28 15:50:50 -0700

    file 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include"main.h"
+#include <stddef.h>
+
+/**
+ * _strstr -entry point,locates a substring
+ * @haystack: -holds value for input
+ * @needle: -holds value for input
+ * Return: (NULL)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	for (i = 0;haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+
+
+		}
+		if (needle[j] == '\0')
+		{
+			return (heystack + i);
+		}
+	}
+	return (NULL);
+}
