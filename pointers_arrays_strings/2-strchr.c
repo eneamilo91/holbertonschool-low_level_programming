06ae41a3a270141f0939be8e5c5e772d3bb87ed8
eneamilo514 <eneamilo514@gmail.com>
2023-10-28 08:11:50 -0700

    file 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include"main.h"
+#include <stddef.h>
+
+/**
+ * _strchr -entry point searches for character in str
+ * @s: -string array to hold values
+ * @c; -holds value for output
+ * Return: NULL
+ */
+
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
