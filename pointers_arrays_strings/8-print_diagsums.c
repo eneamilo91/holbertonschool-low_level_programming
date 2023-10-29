3edda28a8dce880f2a0fa37011602c713a26fba9
eneamilo514 <eneamilo514@gmail.com>
2023-10-29 14:04:58 -0700

    file 8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int j;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				sum1 = sum1 + a[i][j];	
+			}
+			else if (i == size - j)
+			{
+				sum2 = sum2 + a[i][j];
+			}
+		}
+
+	}
+	printf("%d, %d", sum1,sum2);
+}
