7ffeb461d6c7f79217310a772d1c0f5c30748be9
eneamilo514 <eneamilo514@gmail.com>
2023-10-29 13:48:04 -0700

    file 7

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
