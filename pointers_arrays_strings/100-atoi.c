5df20dc78e5519b19b49e447ca2e014e8e02be7d
eneamilo514 <eneamilo514@gmail.com>
2023-10-29 09:49:56 -0700

    file 100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,7 +3,23 @@
 
 int _atoi(char *s)
 {
+	int i;
+	int sign = 1;
+	int number = 0;
 
-
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+		{
+			sign = -sign;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			number = s[i] - '0' + number * 10;
+		}
+		else
+			break;
+	}
+	return (sign * number);
 
 }
