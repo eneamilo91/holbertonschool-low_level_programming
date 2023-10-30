26fd511e68dd61c5f4cd50c0bbe5bc13096b0eb9
eneamilo514 <eneamilo514@gmail.com>
2023-10-30 14:43:07 -0700

    file 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * _pow_recursion -entry point,return x in pow of y
+ * @x: -holds value for output
+ * @y: -holds value for output
+ * Return: (x * _pow_recursion(x, y - 1))
+ */ 
+
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
