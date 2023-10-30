45238fb3e7e203da342814810231a75c97040aa6
eneamilo514 <eneamilo514@gmail.com>
2023-10-30 03:55:23 -0700

    file 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * _puts_recursion -entry point,prints string
+ * @s: -holds value for output
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_putchar(s + 1);
+	}
+	else
+		_putchar('\n');
+}
