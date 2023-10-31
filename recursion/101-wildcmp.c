db9bf0fd3d79ec980e1eead1238990f58cf6e300
eneamilo514 <eneamilo514@gmail.com>
2023-10-31 03:01:58 -0700

    file 8

---
+++ recursion/101-wildcmp.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 != *s2)
+		return (0);
+	if (*(s1 + 1) != '\0' || *(s2 +1) != '\0')
+		wildcmp(s1 +1 , s2 + 1);
+
+	return (1);
+}
