4d4f038a3dac3326131a27f8e256cb847cbe28e1
eneamilo514 <eneamilo514@gmail.com>
2023-10-31 04:52:19 -0700

    s

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -4,7 +4,7 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 != *s2)
+	if ((*s1 != *s2) || !(*s1 == '*' || *s2 != '*'))
 		return (0);
 	if (*(s1 + 1) != '\0' || *(s2 +1) != '\0')
 		wildcmp(s1 +1 , s2 + 1);
