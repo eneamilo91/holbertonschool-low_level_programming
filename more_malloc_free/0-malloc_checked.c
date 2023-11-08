6e843df8a77f4a80e5fdbb67245e2b92d853689c
eneamilo514 <eneamilo514@gmail.com>
2023-11-08 02:54:40 -0800

    file 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include"main.h"
+#include<stdlib.h>
+
+/**
+ * malloc_chacked -entry point,allocates memory using malloc
+ * @b: -holds value for output
+ * Return: (ptr)
+ */
+
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include<stdlib.h>
+
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr = malloc(nmemb * size);
+
+
+
+
+
+}
