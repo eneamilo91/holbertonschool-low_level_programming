41eb9521afc71c7b7b1501b93b562304ccc5df6d
eneamilo514 <eneamilo514@gmail.com>
2023-11-09 11:06:12 -0800

    file 3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,29 +1,27 @@
-#include"main.h"
-#include<stdlib.h>
-
-
+#include "main.h"
+#include <stdlib.h>
 /**
- * array_range -entry point,creates an array of integers
- * @min: -holds value for output
- * @max: -holds value for output
- * Return: (a)
+ * array_range - sdfsd
+ * @min:sdfsdf
+ * @max:sdfsd
+ * Return:sdfgs
  */
-
 
 int *array_range(int min, int max)
 {
+	int *ptr;
 	int i;
-	int *a = malloc((max - min + 1) * sizeof(int));
+	int len = 0;
 
 	if (min > max)
 		return (NULL);
-	if (a == NULL)
-		return (NULL);
+	len = max - min + 1;
+	ptr = malloc(sizeof(int) * len);
 
-	for (i = 0; i < max - min + 1; i++)
-	{
-		*(a + i) = min;
-		min = min + 1;
-	}
-	return (a);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
+
+	return (ptr);
 }
