ea757f9b21c54896a912715e1b1a3cbbde27e61f
eneamilo514 <eneamilo514@gmail.com>
2023-11-10 02:51:47 -0800

    file 2

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    int i;
-    int j;
-
-    i = ABS(-98) * 10;
-    j = ABS(98) * 10;
-    printf("%d, %d\n", i, j);
-    return (0);
+	printf("%s\n", __FILE__);
+		return (0);
 }
