3af14b7c6913cf821118188733adb36b167c24c7
eneamilo514 <eneamilo514@gmail.com>
2023-11-13 01:38:14 -0800

    file 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
+
+/**
+ * struct dog -dog data
+ * @name: -points to specific data
+ * @age: -points to specific data
+ * @owner -points to specific data
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+
+#endif 
