fe5dfb69b64345a93d06def5df5a37329d9f7838
eneamilo514 <eneamilo514@gmail.com>
2023-11-13 04:40:43 -0800

    file 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @owner: -points to specific data
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
