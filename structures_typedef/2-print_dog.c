cc365b7090bd2e05d9fb028f28b4e259ac13dc3d
eneamilo514 <eneamilo514@gmail.com>
2023-11-13 04:09:35 -0800

    file 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include"dog.h"
+
+/**
+ * print_dog -entry point,function that prints a struct
+ * @d: -points to repective address
+ */
+
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)");
+	}
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)");
+	}
+	else
+		printf("Owner: %s\n", d->owner);
+	printf("Age: %f\n", d->age);
+}
