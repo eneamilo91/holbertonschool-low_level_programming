2d7f22f2dda252baa8152c7e5cd886a04eb0be22
eneamilo514 <eneamilo514@gmail.com>
2023-11-13 04:11:34 -0800

    file 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -19,6 +19,7 @@ void print_dog(struct dog *d)
 	}
 	else
 		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 	{
@@ -26,5 +27,5 @@ void print_dog(struct dog *d)
 	}
 	else
 		printf("Owner: %s\n", d->owner);
-	printf("Age: %f\n", d->age);
+
 }
