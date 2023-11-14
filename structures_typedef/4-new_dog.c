51346888776176eb0d972a5e2e433b99665d0d8f
eneamilo514 <eneamilo514@gmail.com>
2023-11-14 01:09:46 -0800

    file 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -20,23 +20,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (my_dog == NULL)
 		return (NULL);
 
-	my_dog->name = strdup(name);
+
 	if (my_dog->name == NULL)
 	{
 		free(my_dog->name);
 		return (NULL);
 	}
 
-	my_dog->age = age;
-	my_dog->owner = strdup(owner);
 	if (my_dog->owner == NULL)
 	{
 		free(my_dog->name);
-		free(my_dog->owner);
 		free(my_dog);
 		return (NULL);
 	}
-
+	my_dog->name = strdup(name);
+	my_dog->age = age;
+	my_dog->owner = strdup(owner);
 	return (my_dog);
 
 }
