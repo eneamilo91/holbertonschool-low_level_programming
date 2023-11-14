0568d8e6b6bec35ad5ef92a706f507ebcf74a632
eneamilo514 <eneamilo514@gmail.com>
2023-11-14 14:27:35 -0800

    file 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include"function_pointers.h"
+#include<stddef.h>
+
+
+/**
+ * int_index -entry point,searches for an int
+ * @array: -points to the respective array
+ * @size: -holds value for output
+ * @cmp: -points to the respective func
+ * Return: (i)
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (cmp == NULL || array == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+
+		if (cmp(*(array + i)))
+			break;
+	}
+		if (i == size - 1)
+			return (-1);
+
+	return (i);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,5 +2,5 @@
 #define MY_HEADER_H
 
 void print_name(char *name, void (*f)(char *));
-
+int int_index(int *array, int size, int (*cmp)(int));
 #endif
