14b8a7e3631fe02b89f343adda203d9e222ff622
eneamilo514 <eneamilo514@gmail.com>
2023-11-14 09:32:44 -0800

    file 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,23 @@
+#include"function_pointers.h"
+#include<stdio.h>
+
+/**
+ * f -entry point,creates string
+ * @str: -points to the respective array
+ */
+
+void f(char *str)
+{
+	printf("%s\n", str);
+}
+
+/**
+ * print_name -entry point,prints a name
+ * @name: -points to the respective value
+ * @f: -points to the respective function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
