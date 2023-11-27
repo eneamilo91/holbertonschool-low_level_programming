36372317557b3da8b28fe67b14eeaff43e1ed181
eneamilo514 <eneamilo514@gmail.com>
2023-11-27 15:24:59 -0800

    file 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include"lists.h"
+#include<stdio.h>
+#include<stddef.h>
+
+/**
+ * print_list -prints singly linked list
+ * @h: -points to the respective structure
+ * Return: (count)
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	int len;
+
+	while (h != NULL)
+	{
+		count++;
+
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+			for (len = 0; h->str[len] != '\0'; len++)
+			{
+
+			}
+
+			printf("[%d] [%s]\n", len, h->str);
+		}
+		h = h->next;
+	}
+	return (count);
+}
---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#include<stddef.h>
+
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif
