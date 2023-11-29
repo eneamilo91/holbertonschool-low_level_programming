f0a0a1b36140759481eeda959ad31758912a1c24
eneamilo514 <eneamilo514@gmail.com>
2023-11-29 08:46:05 -0800

    file 3

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,35 @@
+#include"lists.h"
+#include<stdlib.h>
+
+/**
+ * add_dnodeint_end -adds new nod at the end
+ * @head: -points to the respective structure
+ * @n: -holds value for output
+ * Return: (new)
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *virtual = *head;
+
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		new->prev = NULL;
+		new->next = NULL;
+	}
+	virtual = *head;
+	while (virtual->next != NULL)
+	{
+		virtual = virtual->next;
+	}
+	virtual->next = new;
+	new->next = NULL;
+	new->prev = virtual;
+	return (new);
+}
