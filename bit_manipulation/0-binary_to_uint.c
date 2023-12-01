632b03d2a62bd9dde90bf0ba2fa633f7476ee8d6
eneamilo514 <eneamilo514@gmail.com>
2023-12-01 02:30:24 -0800

    file 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include"main.h"
+#include<string.h>
+
+/**
+ * binary_to_uint -converts binary to int
+ * @b: -points to correspondin bit
+ * Return: (number)
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int number = 0;
+	int len;
+
+	len = strlen(b) - 1;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		if (b[i] == '1')
+			number = number + (1 << len);
+		len--;
+	}
+	return (number);
+}
---
+++ bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef MY_HEADER_FILE_H
+#define MY_HEADER_FILE_H
+
+unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+
+#endif
