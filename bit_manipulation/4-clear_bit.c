9a4ebeab9bf5760de5892c77bf59f4458d854399
eneamilo514 <eneamilo514@gmail.com>
2023-12-01 06:07:58 -0800

    file 4

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,7 +1,21 @@
 #include"main.h"
 
+/**
+ * clear_bit -changes value of bit
+ * @n: -points to number
+ * @index: -holds value for output
+ * Return: (1)
+ */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	
+	unsigned long int helper = *n;
+
+	if (index > sizeof(n) * 8)
+		return (-1);
+
+	if ((helper >> index & 1) == 1)
+		*n = *n ^ 1 << index;
+
+	return (1);
 }
