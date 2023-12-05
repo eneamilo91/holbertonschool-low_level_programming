c985534a310261accaa012ce63d77bc3622a8129
eneamilo514 <eneamilo514@gmail.com>
2023-12-05 04:51:47 -0800

    file 2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include"main.h"
+#include<unistd.h>
+#include<string.h>
+#include<fcntl.h>
+/**
+* append_text_to_file -appends text to file
+* @filename: -points to specific filename
+* @text_content: -append specific string
+* Return: (1)
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int size;
+	int written = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd < 0)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		size = strlen(text_content);
+		written = write(fd, text_content, size);
+	}
+
+	if (written < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+	
+	close(fd);
+	return (1);
+}
