686b2129c0b0fcce9a172a86f0e99ce5a8f9a24f
eneamilo514 <eneamilo514@gmail.com>
2023-12-05 02:25:42 -0800

    file 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,14 +13,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	ssize_t readed;
 	ssize_t written;
-	char *buffer = malloc(letters * sizeof(char));
+	char *buffer;
 
-	if (buffer == NULL)
-		return (0);
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+		return (0);
+	buffer = malloc(letters * sizeof(char));
+	if (buffer == NULL)
 		return (0);
 	readed = read(fd, buffer, letters);
 	written = write(1, buffer, readed);
@@ -28,5 +29,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	close(fd);
+	free(buffer);
 	return (written);
 }
