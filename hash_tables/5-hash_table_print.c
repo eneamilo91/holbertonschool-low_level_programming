a1f1ac1ee984edc9ca781211f791b1dfd1bb7b14
eneamilo514 <eneamilo514@gmail.com>
2023-12-06 14:10:02 -0800

    file 5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - function that prints all hash table keys and pairs
+ *@ht: hash table to be printed
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	int i;
+	hash_node_t *helper = NULL;
+	int otherpair = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (otherpair)
+				printf(", ");
+
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			otherpair = 1;
+			if (ht->array[i]->next != NULL)
+			{
+				helper = ht->array[i]->next;
+				while (helper != NULL)
+				{
+					printf(", ");
+					printf("'%s': '%s'", helper->key, helper->value);
+					helper = helper->next;
+				}
+			}
+		}
+	}
+	printf("}\n");
+}
