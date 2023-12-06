659262212f0b3c1b73d621442eb6b16f7c7980a6
eneamilo514 <eneamilo514@gmail.com>
2023-12-06 13:15:05 -0800

    file 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,14 +10,11 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
-
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
-
 	if (table->array == NULL)
 		return (NULL);
-
 	return (table);
 }
