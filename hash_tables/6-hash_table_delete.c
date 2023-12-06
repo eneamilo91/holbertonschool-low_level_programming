a81d78c1edce4a4560120fab16975e3409ba23ae
eneamilo514 <eneamilo514@gmail.com>
2023-12-06 15:07:30 -0800

    file 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete -deletes the hash table
+ * @ht: -points to the hash table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *helper;
+
+	for(i = 0; i < ht->size; i++)
+	{
+		while(ht->array[i] != NULL)
+		{
+			helper = ht->array[i];
+			ht->array[i] = ht->array[i]->next;
+			free(helper->key);
+			free(helper->value);
+			free(helper);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
