2af59a2a92271d6da067ba4a7c560a5b20e980d6
eneamilo514 <eneamilo514@gmail.com>
2024-01-12 04:11:29 -0800

    changes

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * linear_search -searches linearly in an array of integers
+ * @array: -points to the array
+ * @size: -size of the array
+ * @value: -value to be checked
+ * Return: (i)
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+		{
+			printf("Found %d at index: %d\n", value, i);
+			return (i);
+		}
+	}
+	return (-1);
+}
